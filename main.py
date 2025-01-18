fb50470a8715199ef079d13fd2971f660b468205
Sonia Palit <166869989+soniapalit@users.noreply.github.com>
2025-01-17 19:36:34 -0500

    made grid/background class. started 'jump' method.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time;
-import pygame;
+import time
+import pygame
+
 screenW=600
 screenH=400
 
@@ -10,23 +11,59 @@ running=True;
 dt=0;
 
 
-#stuff
+class Background:
+        def __init__(self, char):
+            self.position = 0
+            self.char=char
+            #as the background moves, position gets higher
 
+        def getPosition(self):
+            return self.position
 
+        def setPosition(self, pos):
+            self.position=pos
 
-class skier:
-    def __init__(self,position):
-        self.x = 0;
-        self.y=0;
-        self.position=position
+        def move(self, amount=1):
+            self.position=self.position+amount
+        
+        def jump(self):
+            self.char.jump()
 
-
+class Skier:
+    def __init__(self,positionP):
+        
+        self.position=positionP
+        self.x = self.position.x
+        self.y=self.position.y
+    def jump(self):
+        print("jumpity jump")
+        for i in range(0,10):
+            time.sleep(1)
+            self.y=self.y+1
+        for i in range(0,10):
+            time.sleep(1)
+            self.y=self.y-1
     def getX (self):
         return self.x
     def getY(self):
         return self.y
+    def getPosition(self):
+        return self.position
 
+skiposvector = pygame.Vector2(0,600);
+mySkier = Skier(skiposvector)
+grid = Background(mySkier)
 
+while running:
+    if mySkier.getY()>600:
+        print(mySkier.getY())
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running = False;
 
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_w:
+                grid.jump()
+                print("jump")
 
 pygame.quit()

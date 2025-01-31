a108c0d330f7a874a76e4b0e041a03c8292cdd02
Sonia Palit <166869989+soniapalit@users.noreply.github.com>
2025-01-31 17:45:36 -0500

    changed jump to make it w velocity. started working w background

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import pygame
 
 screenW=600
 screenH=400
+ground = 300
 
 pygame.init();
 screen = pygame.display.set_mode((screenW,screenH));
 clock=pygame.time.Clock()
 running=True;
 dt=0;
+
+##Photo by eberhard grossgasteiger: https://www.pexels.com/photo/snowy-mountain-1287145/
 
 
 class Background:
@@ -31,32 +34,53 @@ class Background:
 
 class Skier:
     def __init__(self,positionP):
-        
+        self.xVelocity = 1
+        self.yVelocity=0
         self.position=positionP
-        self.x = self.position.x
-        self.y=self.position.y
+
     def jump(self):
         print("jumpity jump")
-        for i in range(0,10):
-            time.sleep(1)
-            self.y=self.y+1
-        for i in range(0,10):
-            time.sleep(1)
-            self.y=self.y-1
+        self.yVelocity = -50
+
+       
     def getX (self):
-        return self.x
+        return self.position.x
     def getY(self):
-        return self.y
+        return self.position.y
     def getPosition(self):
         return self.position
 
-skiposvector = pygame.Vector2(0,600);
-mySkier = Skier(skiposvector)
-grid = Background(mySkier)
+    def calculateNextPos(self):
+        self.position.y+=self.yVelocity
+        self.position.x+=self.xVelocity
+        if self.position.y<ground:
+            self.yVelocity+=5
+        if self.position.y>=ground:
+            self.yVelocity = 0
+            self.position.y=ground
+
+
+def drawSkier(skier: Skier):
+    skierRect = pygame.Rect(skier.position, (200,200))
+    pygame.draw.ellipse(screen, "pink" ,skierRect)
+
+background = pygame.transform.scale_by(pygame.image.load("mountainBackground.jpg"), .25)
+
+def drawBackground():
+    screen.blit(background,(0,0))
+
+skiposvector = pygame.Vector2(100,100);
+skiingSonia = Skier(skiposvector)
+grid = Background(skiingSonia)
+
 
 while running:
-    if mySkier.getY()>600:
-        print(mySkier.getY())
+    drawBackground()
+    print(skiingSonia.getX(),skiingSonia.getY())
+    skiingSonia.calculateNextPos()
+    drawSkier(skiingSonia)
+    if skiingSonia.getY()>600:
+        print(skiingSonia.getY())
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running = False;
@@ -64,6 +88,6 @@ while running:
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_w:
                 grid.jump()
-                print("jump")
-
+    time.sleep(.02)
+    pygame.display.flip()
 pygame.quit()

f02ed52eaafcec763191f6cb3fb88255ca8a7a8b
Sonia Palit <166869989+soniapalit@users.noreply.github.com>
2025-03-11 15:04:37 -0400

    did some of the thingies with the thingies for the thingy for movement for terrain thing

--- expression_evaluator.py
+++ expression_evaluator.py
@@ -12,19 +12,15 @@ def eval_op(op_str, op_func, tokens):
         i = tokens.index(op_str)
         ans = op_func(tokens[i-1], tokens[i+1])
         tokens[i-1:i+2] = [ans]
-        return tokens
+    return tokens
 
 def get_value(math_expr):
     # does not support subtraction
     tokens = re.findall("(\\*|/|\\+|-?\\d+\\.\\d+|-?\\d+|pi)", math_expr)
     tokens = [pi if t == "pi" else safe_float(t) for t in tokens]
-    print(tokens)
     tokens = eval_op("*", lambda a, b: a * b, tokens)
-    print(tokens)
     tokens = eval_op("/", lambda a, b: a / b, tokens)
-    print(tokens)
     tokens = eval_op("+", lambda a, b: a + b, tokens)
-    print(tokens)
     
     # should be completely evaluated
     return tokens[0]
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import time
 import pygame
+import expression_evaluator
+import math
 
-screenW=600
-screenH=400
-ground = 300
+screenW=1050 
+screenH=700 
+ground = 550.0
+terrainCounter = 0
+terrainEnd = 81.38495
+
 
 pygame.init()
 screen = pygame.display.set_mode((screenW,screenH))
@@ -41,7 +46,7 @@ class Skier:
         self.position=positionP
 
     def jump(self):
-        print("jumpity jump")
+        #print("jumpity jump")
         self.yVelocity = -50
 
        
@@ -53,6 +58,8 @@ class Skier:
         return self.position
 
     def calculateNextPos(self):
+
+        firstY = self.position.y
         self.position.y+=self.yVelocity
         self.position.x+=self.xVelocity
         if self.position.y<ground:
@@ -61,6 +68,11 @@ class Skier:
             self.yVelocity = 0
             self.position.y=ground
 
+        yNow = self.position.y
+        if abs(firstY-yNow)>=10:
+            print(f"counter:{terrainCounter}")
+
+    
 
 def drawSkier(skier: Skier):
     # skierRect = pygame.Rect(skier.position, (200,200))
@@ -68,38 +80,105 @@ def drawSkier(skier: Skier):
     
     screen.blit(skierRect, (skier.position))
 
-background = pygame.transform.scale_by(pygame.image.load("SKISKIYA.png"), .5)
+background = pygame.transform.scale_by(pygame.image.load("SKISKIYA.png"), .8)
 
+# get f as a file object
+#f.readline().split(",")
+
+def get_func(func, ho, vo, sv, sh):
+    ho = expression_evaluator.get_value(ho)
+    vo = expression_evaluator.get_value(vo)
+    sv = expression_evaluator.get_value(sv)
+    sh = expression_evaluator.get_value(sh)
+
+    if func == "sin":
+        def sinx(x):
+            print(f"{sv} * math.sin({sh} * x + {ho}) + {vo}")
+            return sv * math.sin(sh * x + ho) + vo
+        return sinx
+    elif func == "e":
+        return lambda x : sv * math.exp(sh * x + ho) + vo
+    
+
+def getExpression (counter):
+        typee = "sin"
+        ho = "0.0"
+        vo = "0.0"
+        sv = "0.0"
+        sh = "0.0"
+
+        if counter <=6.28318530718:
+            sv="1"
+            sh="1"
+            vo="pi/3"
+            ho="3*pi/2"
+        elif counter <=21.91422:
+            sv="2"
+            sh="0.5"
+            vo="2*pi/3.07"
+            ho="pi/2"
+        elif counter <= 26.40571:
+            sv="4"
+            sh="0.5"
+            vo="2*pi/3.07"
+            ho="pi/2+.54"
+        elif counter <= 31.69019:
+            typee = "e"
+            sv="1"
+            sh="-1"
+            vo="0.1"
+            ho="26.8"
+        elif counter <=81.38495:
+            sv="3"
+            sh="1/3"
+            vo="3.1"
+            ho="9"
+        else:
+            sv="1"
+            sh="1/5"
+            vo="5.1"
+            ho="3*pi/2"
+
+
+        equation = get_func(typee, ho, vo, sv, sh)
+        print(equation)
+        return equation(counter)
+        
 
 def drawBackground(numb):
-    screen.blit(background,(numb,-10))
+    screen.blit(background,(numb,-20))
 
 def moveBackground(numb):
-    if numb<=-1050:
+    if numb<=-(1685):
         numb = 0
     else:
         numb-=10
     return numb
 
-skiposvector = pygame.Vector2(100,-100);
+skiposvector = pygame.Vector2(100,-100)
 skiingSonia = Skier(skiposvector)
 grid = Background(skiingSonia)
 
 
 while running:
+    ground = 550 - 50 * getExpression (terrainCounter)
     drawBackground(num)
-    print(num)
+    terrainCounter+=.1
+    if terrainCounter >= terrainEnd-1:
+        terrainCounter=0
+    
+    #print (expression_evaluator.get_value("sin 1+3*pi/4+ pi/3"))
+    #print(num)
     skiingSonia.calculateNextPos()
     drawSkier(skiingSonia)
     num = moveBackground(num)
-    if skiingSonia.getY()>600:
-        print(skiingSonia.getY())
+    print(f'ypos: {skiingSonia.getY()}')
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running = False;
 
         if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_w:
+            if event.key==pygame.K_w and skiingSonia.getY()==ground:
                 grid.jump()
     time.sleep(.02)
     pygame.display.flip()
